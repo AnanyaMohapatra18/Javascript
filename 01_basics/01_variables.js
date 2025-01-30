59b051e6c4c9c1bc7015c3a6c8102d7f6ec66ef5
AnanyaMohapatra18 <ananyamohapatra0102@gmail.com>
2025-01-30 23:47:31 +0530

    Add files via upload

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,32 @@
+const accountId = 771831 //costant keyword ko lekhne ke liye ek hi tarika hai ki hume (const) likhna padega
+
+//Variables ke liye (let and var) use hota hai
+let accountEmail = "ananya@google.com" //let is recommended becoz the block problem is solved in this
+var accountPassword = "128745"
+/* Prefer not to use var beacause of the issue in block scope({}) & functional scope
+Ex= like agar kisi programmer ne accoundPassword likha aur phir kisine file mai condition deke
+accoundPassword dia toh woh uss se replace ho jata tha.
+Pura remove nhi hua hai bss kuch old code likha jata hai
+*/
+
+accountCity = "Bhubaneswarr" 
+// In js hum variable ko bina (var) and (let) use krr ke bhi likh skte hai but it's not a good practice
+
+let accountState; 
+/* Let agar hume variable ki value malum nhi hai aur hum isse bss declare krr ke chod dete hai toh 
+iska value "undefined" maanti hai javascript
+*/
+//js mai semicolon compulsory nhi hai hum usse na deke bhi chod skte hai..
+
+
+// accountId = 2 // not allowed if already one account is existing because we used (const) keyword there
+
+accountEmail = "an@ananya.com"
+accountPassword = "76874479"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+// Don't need to use console.log again and again for accesing the values we can use console.log([]) , square 
+// bracket ke andar hum sb likh skte hai..
