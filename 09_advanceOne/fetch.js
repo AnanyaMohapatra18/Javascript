b6c226d48616ffba19fd80fe1ee2a746776cbf6b
Ananya Mohapatra <ananyamohapatra0102@gmail.com>
2025-03-26 23:24:13 +0530

    Update fetch.js

--- 09_advanceOne/fetch.js
+++ 09_advanceOne/fetch.js
@@ -86,4 +86,21 @@ response body content.
    
    
 
+/** async function getData() {
+  const url = "https://example.org/products.json";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+ **/
+
+
 // READ MDN DOCUMENTATION FOR MORE INFORMATION
